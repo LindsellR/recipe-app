dad0a892e0dbac928b2ed76f3fc25d3b3a04d87b
LindsellR <lindsellrichard@gmail.com>
2025-09-10 18:04:21 +1000

    Added login authentication to app

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -14,16 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import login_view, logout_view, welcome_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('recipes/', include('recipes.urls')),
+    path("admin/", admin.site.urls),
+    path("", welcome_view, name="welcome"),
+    path("recipes/", include("recipes.urls")),
     path("favourites/", include("favourites.urls")),
-    path('search/', include('search.urls', namespace='search')),
+    path("search/", include("search.urls", namespace="search")),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
